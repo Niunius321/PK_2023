c3319cadd49960651a39041cb03fd002344b1624
Niunius321 <57298670+Niunius321@users.noreply.github.com>
2023-02-01 12:13:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
         //Args args = parseArgs(argc, argv);
         auto graph = Pobieranie_plik("/Users/oskarukleja/Desktop/projektt/projektt/../../studia/ppk/projekt/dane.txt");
-            Zapisz(graph, "/Users/oskarukleja/Desktop/projektt/projektt/wynik.txt", stoi("0"));
+            Zapisz(graph, "/Users/oskarukleja/Desktop/projektt/projektt/wynik.txt", stoi("1"));
     return 0;
 }
 
