8b59eaf26fe765b76fc0ffada30c3d983f67392e
Niunius321 <57298670+Niunius321@users.noreply.github.com>
2023-03-24 15:04:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
-#include "functions.h"
 
-using namespace std;
+#include <iostream>
 
-
-int main(int argc, char* argv[]){
-    std::vector<std::string> data = userData(argc, argv);
-        auto graph = Pobieranie_plik(data[0]);
-            Zapisz(graph, data[1], stoi(data[2]));
-    return 0;
+int main()
+{
+    std::cout << "Hello World!\n";
 }
-
