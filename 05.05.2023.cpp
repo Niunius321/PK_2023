fb28544b25d87ae0d1c8056578a4538a7dcc1ada
Niunius321 <57298670+Niunius321@users.noreply.github.com>
2023-05-05 15:06:41 +0200

    Update 05.05.2023.cpp

--- 05.05.2023.cpp
+++ 05.05.2023.cpp
@@ -2,54 +2,41 @@
 #include <cstdlib>
 
 using namespace std;
-#include<stdio.h>
 class walidator {
 public:
-	virtual bool validate(std::string input) = 0;
+	virtual void validate(std::string) = 0;
 };
 class telefonkom : public walidator {
-	bool validate(std::string input) override {
-		return true;
+	void validate(std::string) override{
+		std::cout<<"telefonkom";
 	}
-
 };
 class kwota : public walidator {
-	bool validate(std::string input) override {
-		return true;
+	void validate(std::string) override {
+		std::cout<< "kwota";
 	}
-
 };
-
 class email : public walidator {
-	bool validate(std::string input) override {
-		return true;
+	void validate(std::string) override {
+		std::cout << "email";
 	}
-
 };
 class telefonstat : public walidator {
-	bool validate(std::string input) override {
-		return true;
+	void validate(std::string) override {
+		std::cout << "telefonstat";
 	}
-
 };
 class liczba_rzymska : public walidator {
-	bool validate(std::string input) override {
-		return true;
+	void validate(std::string) override {
+		std::cout << "liczba_rzymska";
 	}
-
 };
-
 
 int main()
 {
-	walidator** tablica = new walidator*[5];
-	tablica[0] = new telefonkom();
-	tablica[1] = new kwota();
-	tablica[2] = new email();
-	tablica[3] = new telefonstat();
-	tablica[4] = new liczba_rzymska();
-	for (int i = 0; i < sizeof(walidator)-1; i++) {
-		tablica[i]->validate("1");
-	}
+    walidator** tablica = new walidator * [5];
+    tablica[0] = new telefonkom();
+    tablica[0]->validate("535933302");
+
 	return 0;
-} 
+}
